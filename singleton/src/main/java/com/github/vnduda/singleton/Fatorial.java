b6de6e86049afdfc5b6c989d2fa3833dd4359573
vnduda <eduardavieiranetto@gmail.com>
2019-09-11 21:34:00 -0300

    exemplo singleton

---
+++ singleton/src/main/java/com/github/vnduda/singleton/Fatorial.java
@@ -0,0 +1,28 @@
+package com.github.vnduda.singleton;
+
+public final class Fatorial {
+	
+	/**
+	 * Referência para a única instância da classe
+	 */
+	private static final Fatorial INSTANCIA = criaInstancia();
+	
+	private int CalculoFatorial(int valor) {
+		int fat;
+
+		for(int i = 2; i <= valor; i++){
+			fat *= i;
+		}
+		return fat;
+	}
+	
+	private static Fatorial criaInstancia() {
+		return new Fatorial();
+	}
+	
+	public static Fatorial getInstance() {
+		return INSTANCIA;
+	}
+	
+	
+}
---
+++ singleton/src/test/java/com/github/vnduda/singleton/FatorialTest.java
@@ -0,0 +1,14 @@
+package com.github.vnduda.singleton;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+class FatorialTest {
+	
+	@Test
+	void fatorialCorreto() {
+		Fatorial fatorial = Fatorial.getInstance();
+		assertEquals(120, fatorial.CalculoFatorial(5));
+	}
+}
